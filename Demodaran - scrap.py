3ffa4c6b1c3081e6d31e7456457bf151ecea2844
MartinAguilarR <martin.aguilarr@utem.cl>
2021-05-11 18:34:04 +0200

    actualización

--- Demodaran - scrap.py
+++ Demodaran - scrap.py
@@ -14,21 +14,25 @@ driver = webdriver.Chrome(options=chrome_options,
                           executable_path="C:\\Program Files (x86)\\chromedriver.exe")
 
 
+url = 'http://pages.stern.nyu.edu/~adamodar/'
 
-def link(url):
-    driver.get(url)
-    data = {}
-    df = pd.DataFrame(columns=['Links'])
+driver.get(url)
 
-    ids = driver.find_elements_by_xpath('/html/body/table/tbody/tr[5]/td[2]/p[1]') #Definición de elemento para descarga
-    for ii in ids:
-        data['Links'] = ii.get_attribute('href')
-        df = df.append(data, ignore_index=True)
-        print(df)
-    df.to_csv('RiskPrem_OtherMkts_links')
+Data_boton = driver.find_element_by_xpath('/html/body/center/div/table/tbody/tr/td[4]').click()
+data_web = Data_boton.find_element_by_xpath('/html/body/div[1]/div[1]/ul/li[4]/a').click
+
+data = {}
+df = pd.DataFrame(columns=['Links'])
+
+ids = driver.find_elements_by_xpath('/html/body/table/tbody/tr[5]/td[2]/p[1]') #Definición de elemento para descarga
+for ii in ids:
+    data['Links'] = ii.get_attribute('href')
+    df = df.append(data, ignore_index=True)
+    print(df)
+df.to_csv('RiskPrem_OtherMkts_links')
 
 
-link('http://pages.stern.nyu.edu/~adamodar/')
+
 
 #Extracción de documentos
 
